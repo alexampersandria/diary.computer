6ce893c9eca6db5a86170d89f9dd86a1fc3d918a
LIARA <liarabruchmann@gmail.com>
2025-12-02 18:57:34 +0100

    fix: check if open before closing outside click event

--- frontend/src/lib/assemblies/RootNavigation.svelte
+++ frontend/src/lib/assemblies/RootNavigation.svelte
@@ -34,7 +34,9 @@ const toggleRightMenu = () => {
 onClickOutside(
   () => navElements,
   () => {
-    rightMenuOpen = false
+    if (rightMenuOpen) {
+      rightMenuOpen = false
+    }
   },
 )
 
