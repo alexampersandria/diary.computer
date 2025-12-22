fd8b5dbee7301a8d8bbd15480280a04b36c1a4db
LIARA <liarabruchmann@gmail.com>
2025-12-22 10:35:50 +0100

    test: format number tests

---
+++ frontend/src/lib/utils/numbers.spec.ts
@@ -0,0 +1,46 @@
+import { expect, it, describe } from 'vitest'
+import { formatNumber } from './numbers'
+
+describe('formatNumber', () => {
+  it('should format number with two decimal places by default', () => {
+    const result = formatNumber(1234.5)
+    expect(result).toBe('1,234.50')
+  })
+
+  it('should format number with no decimal places', () => {
+    const result = formatNumber(1234.5, {
+      decimals: false,
+    })
+    expect(result).toBe('1,235')
+  })
+
+  it('should format number with two decimal places when decimals is true', () => {
+    const result = formatNumber(1234.5, {
+      decimals: true,
+    })
+    expect(result).toBe('1,234.50')
+  })
+
+  it('should format number with two decimal places when decimals is undefined and there are decimal digits', () => {
+    const result = formatNumber(1234.56, {
+      decimals: undefined,
+    })
+    expect(result).toBe('1,234.56')
+  })
+
+  it('should format number with no decimal places when decimals is undefined and there are no decimal digits', () => {
+    const nodecimals = formatNumber(1234, {
+      decimals: undefined,
+    })
+    const decimals = formatNumber(1234.1, {
+      decimals: undefined,
+    })
+    expect(nodecimals).toBe('1,234')
+    expect(decimals).toBe('1,234.10')
+  })
+
+  it('should format 0.1 correctly', () => {
+    const result = formatNumber(0.1)
+    expect(result).toBe('0.10')
+  })
+})
